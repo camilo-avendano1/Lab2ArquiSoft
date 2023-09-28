6864372d072c663a373e78ecec61b00b6d94b842
camilo <camilo.avendano1@udea.edu.co>
2023-09-28 17:51:16 -0500

    update test

--- src/test/java/com/udea/lab2/Lab2ApplicationTests.java
+++ src/test/java/com/udea/lab2/Lab2ApplicationTests.java
@@ -4,31 +4,66 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class Lab2ApplicationTests {
 
-	@Autowired
-	DataController dataController;
 
-	@Test
-	void healthCheck() {
-		assert dataController.healthCheck().equals("Health Check OK!");
-	}
+		@Autowired
 
-	@Test
-	void version() {
-		assert dataController.version().equals("The actual version is 1.0.0");
-	}
+		DataController dataController;
 
-	@Test
-	void nationLength(){
-		Integer length = dataController.getRandomnNations().size();
-		assert length.equals(10);
-	}
 
-	@Test
-	void currencyLength(){
-		Integer length = dataController.getRandomCurrencies().size();
-		assert length.equals(10);
+
+		@Test
+
+		void health() {
+
+			assertEquals("HEALTH CHECK OK!", dataController.healthCheck());
+
+		}
+
+
+
+		@Test
+
+		void version() {
+
+			assertEquals("The actual version is 1.0.0", dataController.version());
+
+		}
+
+
+
+		@Test
+
+		void nationLength() {
+
+			Integer nationsLength = dataController.getRandomnNations().size();
+
+			assertEquals(10, nationsLength);
+
+		}
+
+
+
+		@Test
+
+		void currenciesLength() {
+
+			Integer currenciesLength = dataController.getRandomCurrencies().size();
+
+			assertEquals(10, currenciesLength);
+
+		}
+
+
+
+
+
+
+
+
+
 	}
-}
